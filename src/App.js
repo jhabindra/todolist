f91eefc3b7d5bb95b7ae6c26256e3b4d51266a62
jhabindra <yogendra@Madhus-MacBook-Air.local>
2023-04-13 06:35:47 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import {useState} from 'react'
+
 function App() {
+
+  const [todoList , setTodoList] = useState([])
+
+  const[newTask,setNewTask] =useState('')
+
+
+  const handlechange = (event) =>{
+    setNewTask(event.target.value)
+  }
+
+  const addTask =()=>{
+    // const newTodoList = [...todoList, newTask]
+    // setTodoList(newTodoList)
+    if(newTask != '')
+    setTodoList([...todoList,newTask])
+    else 
+       alert('Please fill the input box')
+
+  }
+
+
+  const deleteTask = (taskName)=>{
+      const newTodoList =todoList.filter((task)=>{
+        // if(task == taskName){
+        //   return false
+        // }
+        // else{
+        //   return true
+        // }
+
+
+        return task != taskName
+
+       
+      })
+
+      setTodoList(newTodoList)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addTask">
+        <input id='task' onChange={handlechange} name='task'/>
+        <button onClick={addTask}>Add Task</button>
+
+      </div>
+      <div className="list">
+      <table className='table'>
+                <thead>
+                  <tr>
+                    <th>Task</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+         {
+          todoList.map((task)=>{
+            return <div>
+            
+                <tbody>
+                  <tr>
+                    <td>{task}</td>
+                    <td>
+                    <button onClick={()=>deleteTask(task)}>X</button>
+                    </td>
+                  </tr>
+                </tbody>
+            
+              {/* <h1>{task}</h1>
+              <button onClick={()=>deleteTask(task)}>X</button> */}
+            </div>
+          })
+         }
+         </table>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// echo "# todolist" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/jhabindra/todolist.git
+// git push -u origin main
